b139b51d94c39b38d2350a8dd3624abe98617181
Khian Calderon <ckhianvictory@gmail.com>
2025-01-23 07:11:42 +0800

    Created File Dialog

---
+++ File_Dialog.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import filedialog
+
+window = Tk()
+
+def openFile():
+    filepath = filedialog.askopenfilename(
+        initialdir = "\\",
+        title = "Open File Title",
+        filetypes = (("text files", "*.txt"), ("all files", "*.*"))
+    )
+    file = open(filepath, "r")
+    print(file.read())
+    file.close()
+
+button = Button(
+    window,
+    text = "Open",
+    command = openFile
+)
+
+button.pack()
+window.mainloop()
