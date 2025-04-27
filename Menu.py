464d1dc933e2df430df614e457755318809e5b7a
Khian Calderon <ckhianvictory@gmail.com>
2025-04-27 22:32:41 +0800

    Updated Menu

--- Menu.py
+++ Menu.py
@@ -11,15 +11,15 @@ fileMenu = Menu(menu, tearoff=0)
 helpMenu = Menu(menu, tearoff=0)
 
 # Adding menu for FileMenu
-fileMenu.add_command(label="Open")
-fileMenu.add_command(label="Save")
+fileMenu.add_command(label="Open", command=lambda: print("File Opened"))
+fileMenu.add_command(label="Save", command=lambda: print("File Saved"))
 fileMenu.add_separator()
-fileMenu.add_command(label="Exit")
+fileMenu.add_command(label="Exit", command=lambda: print("Exited"))
 
 # Adding menu for helpMenu
-helpMenu.add_command(label="Window Help")
+helpMenu.add_command(label="Window Help", command=lambda: print("Window Help"))
 helpMenu.add_separator()
-helpMenu.add_command(label="Ayuda Help")
+helpMenu.add_command(label="Ayuda Help", command=lambda: print("Ayuda HUHU"))
 
 # Adding menu items to main window
 menu.add_cascade(label="File", menu=fileMenu)
